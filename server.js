725153396e96459df9a64d80cf62e16c951b1272
Edgar Palic <edgar.palic99@gmail.com>
2019-11-29 15:32:02 +0100

    Add files via upload
    removed unused code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mysql = require('mysql');
 const expbs = require('express-handlebars');
 const app = express();
 
@@ -8,33 +7,6 @@ app.set('view engine', 'handlebars');
 app.use(express.static("images"));
 app.use(express.static("css"));
 app.use(express.static("js"));
-
-// Database
-var con = mysql.createConnection({
-    host: '178.16.211.2',
-    user: 'essingen_user',
-    password: 'sfdlfk85',
-    database: 'essingen'
-});
-
-con.connect((err) => {
-    /* 
-    Tables in Essingen DB
-    RowDataPacket { Tables_in_essingen: '__migrationhistory' },
-    RowDataPacket { Tables_in_essingen: 'placecategories' },   
-    RowDataPacket { Tables_in_essingen: 'places' } ]
-    */
-    if (err) throw err;
-
-    // var query = "show tables in essingen"
-    var query = "select * from placecategories"
-    con.query(query, function (err, result, fields) {
-        if (err) throw err;
-        //console.log(result);
-    });
-    console.log('Connection established');
-    con.end();
-});
 
 // Routing
 app.get('/', (req, res) => {
